2d9d47cb17febc8ec739bb36b3f4102b0350db82
Nolanole <Jmancuso82@gmail.com>
2019-05-27 17:49:20 -0500

    added new module containing 2 helper functions, 'date_split' and 'check_nulls'

---
+++ lambdata_Nolanole/helper_functions.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+
+def date_split(X, date_cols):
+  '''
+  Function to split a date feature in pandas dataframe and create 3 new features,
+  day, month, and year
+  '''
+  X = X.copy() 
+  for col in date_cols:
+    X[col] = pd.to_datetime(X[col], infer_datetime_format=True)
+    X[col + '_year'] = X[col].dt.year
+    X[col + '_month'] = X[col].dt.month
+    X[col + '_day'] = X[col].dt.day
+  return X
+
+def check_nulls(X):
+  '''
+  Checks a pandas dataframe for nulls, prints a report stating how many null
+  observations per feature (only of features that contain nulls)
+  '''
+  count = 0
+  for col in X.columns:
+    col_nulls = X[col].isna().sum()
+    if col_nulls != 0:
+      count += 1
+      print('The ' + str(col) + ' feature has ' + str(col_nulls) + ' null observations')
+    else: 
+      continue
+  if count == 0:
+    print('The dataframe does not contain any null observations')
